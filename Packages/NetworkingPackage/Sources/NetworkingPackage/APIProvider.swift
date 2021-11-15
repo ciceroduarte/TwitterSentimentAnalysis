94dcf97928f830a16d44d97c21b99a9a25048b50
Cícero Duarte <cicero.duarte@dextra-sw.com>
2021-11-15 16:16:12 -0300

    Adding NetworkingPackage

---
+++ Packages/NetworkingPackage/Sources/NetworkingPackage/APIProvider.swift
@@ -0,0 +1,51 @@
+//
+//  APIProvider.swift
+//  TwitterSentimentAnalysis
+//
+//  Created by Cicero Duarte on 09/11/21.
+//
+import Foundation
+
+public enum FetchError: Error {
+    case networkError
+    case invalidResponse
+}
+
+open class APIProvider {
+
+    weak var session: URLSession?
+
+    public init(withSession session: URLSession) {
+        self.session = session
+    }
+
+    open func fetch<T: Decodable>(_ representable: T.Type,
+                             request: URLRequest,
+                             completionHandler: @escaping (Result<T, FetchError>) -> Void) {
+
+        session?.dataTask(with: request, completionHandler: { data, _, error in
+            guard error == nil, let data = data else {
+                DispatchQueue.main.async {
+                    completionHandler(.failure(.networkError))
+                }
+                return
+            }
+
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+                let responseValue = try decoder.decode(representable.self, from: data)
+
+                DispatchQueue.main.async {
+                    completionHandler(.success(responseValue))
+                }
+
+            } catch {
+                DispatchQueue.main.async {
+                    completionHandler(.failure(.invalidResponse))
+                }
+            }
+        }).resume()
+    }
+}
---
+++ Packages/NetworkingPackage/Tests/NetworkingPackageTests/APIProviderTests.swift
@@ -0,0 +1,70 @@
+//
+//  APIProviderTests.swift
+//  TwitterSentimentAnalysisTests
+//
+//  Created by Cicero Duarte on 09/11/21.
+//
+import XCTest
+@testable import NetworkingPackage
+
+class APIProviderTests: XCTestCase {
+
+    lazy var request = URLRequest(url: URL(string: "http://www.url.com")!)
+    var session: URLSessionMock?
+
+    func test_fetch_shouldReturnSuccess_whenValidResponse() {
+        let expec = expectation(description: "Should return a valid response")
+        let sut = makeSUT(withData: "[]")
+
+        sut.fetch([String].self, request: request) { result in
+            if case .success = result {
+                expec.fulfill()
+            }
+        }
+        wait(for: [expec], timeout: 0.1)
+    }
+
+    func test_fetch_shouldReturnFetchError_whenInvalidResponse() {
+        let expec = expectation(description: "Should return a valid response")
+        let sut = makeSUT(withData: "{}")
+
+        sut.fetch([String].self, request: request) { result in
+            if case .failure(let error) = result {
+                XCTAssertTrue(error == .invalidResponse)
+                expec.fulfill()
+            }
+        }
+        wait(for: [expec], timeout: 0.1)
+    }
+
+    func test_fetch_shouldReturnNetworkError_whenErrorNotNil() {
+        let expec = expectation(description: "Should return a valid response")
+        let sut = makeSUT(error: FetchError.networkError)
+
+        sut.fetch([String].self, request: request) { result in
+            if case .failure(let error) = result {
+                XCTAssertTrue(error == .networkError)
+                expec.fulfill()
+            }
+        }
+        wait(for: [expec], timeout: 0.1)
+    }
+
+    func test_fetch_shouldReturnNetworkError_whenDataIsNil() {
+        let expec = expectation(description: "Should return a valid response")
+        let sut = makeSUT()
+
+        sut.fetch([String].self, request: request) { result in
+            if case .failure(let error) = result {
+                XCTAssertTrue(error == .networkError)
+                expec.fulfill()
+            }
+        }
+        wait(for: [expec], timeout: 0.1)
+    }
+
+    private func makeSUT(withData data: String? = nil, error: Error? = nil) -> APIProvider {
+        session = URLSessionMock(withData: data?.data(using: .utf8), error: error)
+        return APIProvider(withSession: session ?? URLSessionMock())
+    }
+}
